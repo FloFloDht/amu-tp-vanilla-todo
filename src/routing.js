b0406179c9126bf5b2b7d4cf5cb56dc271edfdfd
FloFloDht <flo.11.italia@gmail.com>
2022-01-11 10:27:26 +0100

    correction affichage details

--- src/routing.js
+++ src/routing.js
@@ -1,11 +1,12 @@
 // src/routing.js
 import { displayTodoList } from "./ui";
+import { displayTodoDetails } from "./ui";
 
 /**
  * Appelle la fonction correspondante à une URL donnée
  * @param {string} url 
  */
-export const applyRouting = (url) => {
+ export const applyRouting = (url) => {
     let params;
 
     // Si l'URL ressemble à /12/details
@@ -13,7 +14,7 @@ export const applyRouting = (url) => {
         // On cherche à afficher le détail d'une tâche
         const id = +params[1];
 
-        console.log("J'affiche la tâche n°" + id);
+        displayTodoDetails(id);
         return;
     }
     // Dans tous les autres cas, on présente la liste des tâches
